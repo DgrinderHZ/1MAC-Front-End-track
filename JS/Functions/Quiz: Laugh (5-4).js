13d90e15d22f1e9509e82c8a2826d0da7e7aa785
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-12-29 01:21:18 +0100

    Create Quiz: Laugh (5-4).js

---
+++ JS/Functions/Quiz: Laugh (5-4).js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(num){
+    haha = "";
+    for(var i = 0; i < num; i++){
+        haha += "ha";
+    }
+    haha += "!";
+return haha;
+}
+
+
+console.log(laugh(10));
